28942d5d8353e6ea687ebb3b9ea01334446fd1b8
Lucas Mendonça <lucas.mendonca@tc.com.br>
2022-08-06 12:55:14 -0300

    feat: add suport for icons

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React from 'react'
+import Icon from 'react-native-vector-icons/Feather'
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from '@screens/Home'
+import Profile from '@screens/Profile'
 
-import Home from '@screens/Home';
-import Profile from '@screens/Profile';
-
-const Tab = createBottomTabNavigator();
+const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            const iconName = route.name === 'Home' ? 'home' : 'user'
+
+            return <Icon name={iconName} size={size} color={color} />
+          },
+          tabBarActiveTintColor: '#fff',
+          tabBarInactiveTintColor: 'rgba(255, 255, 255, 0.3)',
+          tabBarStyle: {
+            backgroundColor: '#121212',
+          },
+        })}
+      >
         <Tab.Screen name='Home' component={Home} />
         <Tab.Screen name='Settings' component={Profile} />
       </Tab.Navigator>
