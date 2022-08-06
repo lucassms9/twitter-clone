b128d4cb3e8bc21421de453d3b7b87f2124e8866
Lucas Mendonça <lucas.mendonca@tc.com.br>
2022-08-06 11:53:35 -0300

    feat: add react navigation setup

---
+++ src/routes/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Home from '../screens/Home';
+import Profile from '../screens/Profile';
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name='Home' component={Home} />
+        <Tab.Screen name='Settings' component={Profile} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
+}
